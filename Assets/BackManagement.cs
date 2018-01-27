176a93d20e0d57327cd771a5b7bb538366175b5b
Aidas Mazaliauskas <aidas0505@gmail.com>
2018-01-28 01:05:38 +0200

    no message

---
+++ Assets/BackManagement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class BackManagement : MonoBehaviour 
+{
+	public string menu;
+	
+	public void Update ()
+	{
+		if (Input.GetKeyDown(KeyCode.Escape))
+		{
+			GoToMenu();
+		}
+	}
+	
+	public void GoToMenu () 
+	{
+		SceneManager.LoadScene(menu);
+	}
+}
